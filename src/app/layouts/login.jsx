7e31abead8024c6205a5f92215124078dcd959cb
slavaOlshanechky <olshanechky@gmail.com>
2022-01-20 23:21:46 +0200

    add forms; control and uncontrol components; handling multiple field values; reusable input field

--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -1,6 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import TextField from "../components/textField";
 
 const Login = () => {
-    return <h1>Login</h1>;
+    const [data, setData] = useState({
+        email: "",
+        password: ""
+    });
+    const handleChange = ({ target }) => {
+        setData(prevState => ({
+            ...prevState,
+            [target.name]: target.value
+        }));
+    };
+    return (
+        <form action="">
+            <TextField label='Электронная почта' name='email' value={data.email} onChange={handleChange}/>
+            <TextField label='Пароль' type='password' name='password' value={data.password} onChange={handleChange}/>
+            {/* <div> */}
+            {/*     <label htmlFor="email">Email</label> */}
+            {/*     <input type="text" id="email" value={data.email} name="email" onChange={handleChange}/> */}
+            {/* </div> */}
+            {/* <div> */}
+            {/*     <label htmlFor="password">Password</label> */}
+            {/*     <input type="password" id="password" value={data.password} name="password" onChange={handleChange}/> */}
+            {/* </div> */}
+        </form>
+    );
 };
 export default Login;
