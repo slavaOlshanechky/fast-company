f6842b78107996c9f6e68e28f21c6c502d1a9848
slavaOlshanechky <olshanechky@gmail.com>
2022-01-24 22:35:05 +0200

    add bootstrap styles; hide/show password

--- src/app/components/textField.jsx
+++ src/app/components/textField.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 const TextField = ({
@@ -9,17 +9,37 @@ const TextField = ({
     onChange,
     error
 }) => {
+    const [showPassword, setShowPassword] = useState(false);
+    const getInputClasses = () => {
+        return "form-control" + error ? " is-invalid" : "";
+    };
+
+    const toggleShowPassword = () => {
+        setShowPassword((prevState) => !prevState);
+    };
     return (
-        <div>
+        <div className="mb-4 ">
             <label htmlFor={name}>{label}</label>
-            <input
-                type={type}
-                id={name}
-                value={value}
-                name={name}
-                onChange={onChange}
-            />
-            {error && <p>{error}</p>}
+            <div className="input-group has-validation">
+
+                <input
+                    type={showPassword ? "text" : type}
+                    id={name}
+                    value={value}
+                    name={name}
+                    onChange={onChange}
+                    className={getInputClasses()}
+                />
+                {type === "password" && (<button
+                        className="btn btn-outline-secondary"
+                        type="button"
+                        onClick={toggleShowPassword}>
+                        <i className={'bi bi-eye'+(showPassword?"-slash":"")}/>
+                </button>
+                )}
+                {error && <div className="invalid-feedback">{error}</div>}
+
+            </div>
         </div>
     );
 };
--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -66,13 +66,22 @@ const Login = () => {
         console.log(e);
     };
     return (
-        <form onSubmit={handleSubmit}>
-            <TextField label="Электронная почта" name="email" value={data.email} onChange={handleChange}
-                       error={errors.email}/>
-            <TextField label="Пароль" type="password" name="password" value={data.password} onChange={handleChange}
-                       error={errors.password}/>
-            <button type="submit" disabled={!isValid}>Submit</button>
-        </form>
+        <div className="container mt-5">
+            <div className="row">
+                <div className=".col-md-6 .offset-md-3 shadow p-4">
+                    <h3 className="mb-4">Login</h3>
+                    <form onSubmit={handleSubmit}>
+                        <TextField label="Электронная почта" name="email" value={data.email} onChange={handleChange}
+                                   error={errors.email}/>
+                        <TextField label="Пароль" type="password" name="password" value={data.password}
+                                   onChange={handleChange}
+                                   error={errors.password}/>
+                        <button type="submit" disabled={!isValid} className="btn btn-primary w-100 mx-auto">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+
     );
 };
 export default Login;
