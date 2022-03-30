b097ae9ba30f11d6a571b92f1e1835f4ec0d4c46
slavaOlshanechky <olshanechky@gmail.com>
2022-03-30 15:57:14 +0300

    Add link to firebase. Axios intercepters. Request. URL modification

--- src/app/services/http.service.js
+++ src/app/services/http.service.js
@@ -1,8 +1,21 @@
 import axios from "axios";
 import {toast} from "react-toastify";
-import config from "../config.json"
+import configFile from "../config.json"
 
-axios.defaults.baseURL = config.apiEndPoint
+axios.defaults.baseURL = configFile.apiEndPoint
+
+axios.interceptors.request.use(function (config) {
+    // Do something before request is sent
+    if(configFile.isFireBase){
+        const containSlash=/\/$/gi.test(config.url)
+        config.url=(containSlash?config.url.slice(0,-1):config.url)+'.json'
+        console.log(config.url);
+    }
+    return config;
+}, function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+});
 
 axios.interceptors.response.use((res) => res, function (error) {
     const expectedErrors = error.response &&
