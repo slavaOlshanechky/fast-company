06e97a85bbffd7b92537ca16648eaf1a30b54039
slavaOlshanechky <olshanechky@gmail.com>
2022-04-06 15:36:55 +0300

    getting and saving User data

--- src/app/services/http.service.js
+++ src/app/services/http.service.js
@@ -20,9 +20,9 @@ http.interceptors.request.use(function (config) {
 });
 
 function transformData(data) {
-    return data ? Object.keys(data).map(key => ({
+    return data&&!data._id ? Object.keys(data).map(key => ({
         ...data[key]
-    })) : [];
+    })) : data;
 }
 
 http.interceptors.response.use(
