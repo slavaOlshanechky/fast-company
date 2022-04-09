7812210a90cd17e81e9f4be733ecc7b71caa4f11
slavaOlshanechky <olshanechky@gmail.com>
2022-04-09 22:42:56 +0300

    set unchangеable avatar for user

--- src/app/components/ui/navProfile.jsx
+++ src/app/components/ui/navProfile.jsx
@@ -13,11 +13,9 @@ const NavProfile = () => {
         <div className="dropdown" onClick={toggleMenu}>
             <div className="btn dropdown-toggle d-flex align-items-center">
                 <div className="me-2">{currentUser.name}</div>
-                <img src={`https://avatars.dicebear.com/api/avataaars/${(
-                    Math.random() + 1
-                )
-                    .toString(36)
-                    .substring(7)}.svg`} alt="" className="img-responsive rounded-circle"
+                <img src={currentUser.image}
+                     alt=""
+                     className="img-responsive rounded-circle"
                      height="40"/>
             </div>
             <div className={"w-100 dropdown-menu" + (isOpen ? " show" : "")}>
