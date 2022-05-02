9c4bf756652b649ef4f2233ec4b988440de5e0ac
slavaOlshanechky <olshanechky@gmail.com>
2022-05-03 00:00:19 +0300

    add qualitiesRequested,qualitiesReceived,qualitiesRequestFailed,loadQualitiesList

--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./components/ui/navbar";
 import { Redirect, Route, Switch } from "react-router-dom";
 import Main from "./layouts/main";
@@ -10,24 +10,31 @@ import { QualityProvider } from "./hooks/useQualities";
 import AuthProvider from "./hooks/useAuth";
 import ProtectedRoute from "./components/common/protectedRoute";
 import LogOut from "./layouts/logout";
+import { useDispatch } from "react-redux";
+import { loadQualitiesList } from "./store/qualities";
 
 function App() {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(loadQualitiesList());
+    }, []);
+
     return (
         <div>
             <AuthProvider>
-            <Navbar/>
-            <QualityProvider>
-                <ProfessionProvider>
-                    <Switch>
-                        <Route path="/" exact component={Main}/>
-                        <Route path="/login/:type?" component={Login}/>
-                        <Route path="/logout" component={LogOut}/>
-                        <ProtectedRoute path="/users/:userId?/:edit?" component={Users}/>
-                        <Redirect to="/"/>
-                    </Switch>
-                </ProfessionProvider>
-            </QualityProvider>
-            {/* <Route path="/users/:userId?" component={Users} /> */}
+                <Navbar/>
+                <QualityProvider>
+                    <ProfessionProvider>
+                        <Switch>
+                            <Route path="/" exact component={Main}/>
+                            <Route path="/login/:type?" component={Login}/>
+                            <Route path="/logout" component={LogOut}/>
+                            <ProtectedRoute path="/users/:userId?/:edit?" component={Users}/>
+                            <Redirect to="/"/>
+                        </Switch>
+                    </ProfessionProvider>
+                </QualityProvider>
+                {/* <Route path="/users/:userId?" component={Users} /> */}
             </AuthProvider>
             <ToastContainer/>
         </div>
